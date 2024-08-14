194ed92c645c2e91d7bd2aaa4cb61c55075ca38b
Rodrigue Danso <onecodeaday.ocad@gmail.com>
2024-08-14 18:12:29 +0100

    Code routes controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+categories = %w[chinese italian japanese french belgian]
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: categories.sample
+  )
+puts "created restarant > #{restaurant.name}"
+end
+
+puts 'Creating reviews...'
+10.times do
+  review = Review.create!(content: Faker::Quotes::Shakespeare.hamlet_quote,
+    rating: rand(1..5), restaurant_id: Restaurant.all.sample.id
+  )
+puts "created review > #{review.content}"
+end
+
+puts "Finished!"
